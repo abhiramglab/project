18092961619955a576f3a58b2b9ac433c2aa2583
abhiramglab <gottumukkala.cs18@bmsce.ac.in>
2020-11-03 13:14:34 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 400
+      }
+    }
+  }
+}
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <Particles className='particles'
+         params={particlesOptions}
+        />
+        <Navigation />
+        <Signin />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
